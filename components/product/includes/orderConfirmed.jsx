03252f65c0bb75e1ac253a6f720f6d5c93a8ca91
Maranatha A. Ilesanmi <maranatha.ilesanmi@andela.com>
2018-07-01 09:51:40 +0100

    Revert "Fix heroku deploy"

--- components/product/includes/orderConfirmed.jsx
+++ components/product/includes/orderConfirmed.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const OrderConfirmed = () => (
   <div className="col-md-9 col-md-push-3">
     <div className="row">
-      <h1>Order Confirmed!</h1>
+      <h1>Order Confirmed</h1>
       Your order has been confirmed and is being processed.
       Click <Link to='/user/orders'>here</Link> to view your orders.
     </div>
