ba9378e6c57e088bb6b760927c016ee9f31c5536
seunkoko <seunowonikoko@gmail.com>
2018-07-06 13:44:45 +0100

    Fix export order id

--- components/includes/Header.jsx
+++ components/includes/Header.jsx
@@ -29,14 +29,15 @@ class Header extends Component {
       <header className="header sticky-header">
         <div className="container">
           <Link to="/" className="site-logo" title="Andela Sell It">
-            <img
-               src="/assets/images/andela-logo.png"
-               width={100}
-               height={48}
-               alt="Logo"
-             />
-            <span className="sr-only">Andela Sell It</span>
-          </Link>
+              <img
+                src="/assets/images/andela-logo.png"
+                width={100}
+                height={48}
+                alt="Logo"
+              />
+              <span className="sr-only">Andela Sell It</span>
+            </Link>
+
           <div className="header-dropdowns">
             <div className="dropdown header-dropdown">
                <a className="dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -44,28 +45,29 @@ class Header extends Component {
             <i className="fa fa-caret-down" />
           </a>
                <ul className="dropdown-menu">
-               <li><Link to="#" onClick={() => { this.setLocation('Lagos'); location.reload() }} title="Spanish">Lagos</Link></li>
-            <li><Link to="#" onClick={() => { this.setLocation('Nairobi'); location.reload() }} title="Spanish">Nairobi</Link></li>
-            <li><Link to="#" onClick={() => { this.setLocation('Kampala'); location.reload() }} title="Turkish">Kampala</Link></li>
-            <li><Link to="#" onClick={() => { this.setLocation('NewYork'); location.reload() }} title="German">New York</Link></li>
-          </ul>
+                <li><Link to="#" onClick={() => { this.setLocation('Lagos'); location.reload() }} title="Spanish">Lagos</Link></li>
+                <li><Link to="#" onClick={() => { this.setLocation('Nairobi'); location.reload() }} title="Spanish">Nairobi</Link></li>
+                <li><Link to="#" onClick={() => { this.setLocation('Kampala'); location.reload() }} title="Turkish">Kampala</Link></li>
+                <li><Link to="#" onClick={() => { this.setLocation('NewYork'); location.reload() }} title="German">New York</Link></li>
+              </ul>
              </div>{/* End .dropddown */}
           </div>
-
-          <SearchBar />
-          {/* End .search-form-container */}
-          <ul className="top-links">
-            <li>
-               <Link to="/products/create">Sell A Product</Link>
-             </li>
-            {/* <li><a href="cart.html">Cart</a></li> */}
-          </ul>
 
           <a href="#" className="sidemenu-btn" title="Menu Toggle">
             <span />
             <span />
             <span />
           </a>
+
+          <SearchBar />
+          {/* End .search-form-container */}
+
+          <ul className="top-links">
+            <li>
+               <Link to="/products/create" id="sell-product">Sell A Product</Link>
+             </li>
+          </ul>
+          
         </div>{/* End .container-fluid */}
       </header>
     );
