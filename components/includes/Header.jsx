222ed31bea8cce6d92ceef48a5ac9ce82e192471
Maranatha A. Ilesanmi <maranatha.ilesanmi@gmail.com>
2018-07-13 21:16:25 +0100

    Fix bug with location change

--- components/includes/Header.jsx
+++ components/includes/Header.jsx
@@ -48,7 +48,7 @@ class Header extends Component {
                 <li><Link to="#" onClick={() => { this.setLocation('Lagos'); location.reload() }} title="Spanish">Lagos</Link></li>
                 <li><Link to="#" onClick={() => { this.setLocation('Nairobi'); location.reload() }} title="Spanish">Nairobi</Link></li>
                 <li><Link to="#" onClick={() => { this.setLocation('Kampala'); location.reload() }} title="Turkish">Kampala</Link></li>
-                <li><Link to="#" onClick={() => { this.setLocation('NewYork'); location.reload() }} title="German">New York</Link></li>
+                <li><Link to="#" onClick={() => { this.setLocation('New York'); location.reload() }} title="German">New York</Link></li>
               </ul>
              </div>{/* End .dropddown */}
           </div>
